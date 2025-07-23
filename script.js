d06c3abe2e1e3a96019696320cbf7939bb457bf4
Equiarh1234 <priscillaallotey744@gmail.com>
2025-07-23 10:43:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,5 +19,21 @@ this .reset()
 
 }
 )
-   
+//show popup after 5 seconds
+
+setTimeout(function(){
+    document.getElementById("emmail-popup").style.display="flex"
+},5000);
+
+// close popup when X is clicked
+document.querySelector(".close-btn").addEventListener("click",function(){
+    document.getElementById("emmail-popup").style.display="none";
+})
+
+// close when clicking outside content
+document.getElementById("emmail-popup").addEventListener("click",function(e){
+    if(e.target=== this){
+        this.style.display="none";
+ }
+});   
     
